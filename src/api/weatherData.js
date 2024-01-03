7f40618ada144016606b8fb070cc819c20c689fb
DevSaLLein <isaac.andrade3@aluno.ce.gov.br>
2024-01-03 11:18:16 -0300

    remove bug

--- src/api/weatherData.js
+++ src/api/weatherData.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
 const KEY = 'baf2c084012240e7a9a154834232909';
-const url = 'http://api.weatherapi.com/v1/current.json';
+const url = 'https://api.weatherapi.com/v1/current.json';
 
 const fetchData = async (city) => {
     const req = `${url}?key=${KEY}&q=${city}&aqi=no`;
